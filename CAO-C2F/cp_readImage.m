9a7dbcf1c1362b80d962c9f1aa9dab4fc755b65f
JiangQian <48743320+sjtu-jq@users.noreply.github.com>
2021-03-18 11:24:10 +0800

    Update cp_readImage.m

--- CAO-C2F/cp_readImage.m
+++ CAO-C2F/cp_readImage.m
@@ -1,16 +1,20 @@
 
-function [I1_gray, I2_gray, I1_rgb, I2_rgb, Filename1, Filename2, pathname] = cp_readImage(distort,path,file1,file2)
+function [I1_gray, I2_gray, I1_rgb, I2_rgb, Filename1, Filename2, pathname] = cp_readImage(distort,inputpath,file1,file2)
 
 %%
 clc
 close all
 %% Read images needed to be registrated
-disp('¡¾0¡¿Reading images ...')
+disp('ã€0ã€‘Reading images ...')
 filenameset = ['JPG' 'PNG' 'TIF' 'BMP' 'PEG' 'PPM' 'PGM'...
                'jpg' 'png' 'tif' 'bmp' 'peg' 'ppm' 'pgm' 'dat'];
+ptemp = matlabpath;
+ind = strfind(ptemp,';');
+filepath = ptemp(ind(end)+1:end);
+
 if nargin == 0
     distort = 0;
-    [Filename1,pathname] = uigetfile('*.*','pick image1');
+    [Filename1,pathname] = uigetfile([filepath '\*.*'],'pick an infrared image');
     if ~contains(filenameset,Filename1(end-2:end))
         error('Incorrect file format input!');
     end
@@ -18,11 +22,11 @@ if nargin == 0
         [I1,I11,I2,pathname,Filename1,Filename2] = cp_readTempMat(pathname,Filename1);
     else
         I1 = imread([pathname Filename1]);
-        [Filename2,pathname] = uigetfile([pathname '*' Filename1(end-3:end)],'pick image2');
+        [Filename2,pathname] = uigetfile([pathname '*' Filename1(end-3:end)],'pick a visible image');
         I2 = imread([pathname Filename2]);
     end
 elseif nargin == 1
-    [Filename1,pathname] = uigetfile('*.*','pick image1');
+    [Filename1,pathname] = uigetfile([filepath '\*.*'],'pick an infrared image');
     if ~contains(filenameset,Filename1(end-2:end))
         error('Incorrect file format input!');
     end
@@ -30,18 +34,20 @@ elseif nargin == 1
         [I1,I11,I2,pathname,Filename1,Filename2] = cp_readTempMat(pathname,Filename1);
     else
         I1 = imread([pathname Filename1]);
-        [Filename2,pathname] = uigetfile([pathname '*' Filename1(end-3:end)],'pick image2');
+        [Filename2,pathname] = uigetfile([pathname '*' Filename1(end-3:end)],'pick a visible image');
         I2 = imread([pathname Filename2]);
     end
 elseif nargin  == 4
-    I1 = imread([path file1]);
-    I2 = imread([path file2]);
-    pathname = path;
+    I1 = imread([inputpath file1]);
+    I2 = imread([inputpath file2]);
+    pathname = inputpath;
     Filename1 = file1;
     Filename2 = file2;
 else
     error('No sufficient input parameters!');
 end
+
+path(path, pathname(1:end-1));
 
 if distort
     if size(I1,1)~=768 && size(I1,1)>288 && size(I1,1)<1080 && size(I2,1)==1190
@@ -61,7 +67,7 @@ if distort
         disp('   Undistort image successfully!');
     end
 end
-fprintf('\tsize of Image 1 is [%d¡Á%d]\n    size of Image 2 is [%d¡Á%d]\n',size(I1,1),size(I1,2),size(I2,1),size(I2,2));
+fprintf('\tsize of Image 1 is [%dÃ—%d]\n    size of Image 2 is [%dÃ—%d]\n',size(I1,1),size(I1,2),size(I2,1),size(I2,2));
 if size(I1,3)==3 & size(I2,3)==3 
         I1_rgb = I1;
         I2_rgb = I2;
@@ -83,11 +89,7 @@ else
         I1_rgb = 0;
         I2_rgb = 0;
 end
-% figure, imshow(uint8(I1_gray));
 I1_gray = uint8((I1_gray-min(min(I1_gray)))./(max(max(I1_gray))-min(min(I1_gray))) * 255);
 I2_gray = uint8((I2_gray-min(min(I2_gray)))./(max(max(I2_gray))-min(min(I2_gray))) * 255);
-% figure, imshow(I1_gray);
-disp('¡¾0¡¿Completed to read image£¡')
 
-
-
+disp('ã€0ã€‘Completed to read imageï¼')
